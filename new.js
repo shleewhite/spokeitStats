b69cf00528c91087932b7a589a05b287a34ba53e
shleewhite <leewhite128@gmail.com>
2017-05-09 16:04:22 -0700

    made changes

--- new.js
+++ new.js
@@ -41,6 +41,25 @@ var try3_diff6 = []						// array of levels attempted at difficulty 6
 var try3_diff7 = []						// array of levels attempted at difficulty 7
 var try3_diff8 = []						// array of levels attempted at difficulty 8 (HARDEST)
 
+var h_perf = []                         // arrays for performance based on what said
+var p_perf = []
+var b_perf = []
+var t_perf = []
+var d_perf = []
+var k_perf = []
+var g_perf = []
+var s_perf = []
+var th_perf = []
+var ths_perf = []
+var sh_perf = []
+var f_perf = []
+var ch_perf = []
+var j_perf = []
+var nt_perf = []
+var nd_perf = []
+var lt_perf = []
+var ld_perf = []
+
 // adds all dates in January and February to dates array
 for (var i = 1; i < 32; i++) { dates.push("Jan " + i + ", 2016"); }
 for (var i = 1; i < 29; i++) { dates.push("Feb " + i + ", 2016"); }
@@ -94,6 +113,29 @@ for (var i = 0; i < dates.length; ++i) {
 	try3_diff8.push(total);
 }
 
+// add values to performance arrays
+for (var i = 0; i < dates.length; ++i) {
+    h_perf.push((Math.random()*8)*(Math.random()*4))
+    p_perf.push((Math.random()*8)*(Math.random()*4))
+    b_perf.push((Math.random()*8)*(Math.random()*4))
+    t_perf.push((Math.random()*8)*(Math.random()*4))
+    d_perf.push((Math.random()*8)*(Math.random()*4))
+    k_perf.push((Math.random()*8)*(Math.random()*4))
+    g_perf.push((Math.random()*8)*(Math.random()*4))
+    s_perf.push((Math.random()*8)*(Math.random()*4))
+    th_perf.push((Math.random()*8)*(Math.random()*4))
+    ths_perf.push((Math.random()*8)*(Math.random()*4))
+    sh_perf.push((Math.random()*8)*(Math.random()*4))
+    f_perf.push((Math.random()*8)*(Math.random()*4))
+    ch_perf.push((Math.random()*8)*(Math.random()*4))
+    j_perf.push((Math.random()*8)*(Math.random()*4))
+    nt_perf.push((Math.random()*8)*(Math.random()*4))
+    nd_perf.push((Math.random()*8)*(Math.random()*4))
+    lt_perf.push((Math.random()*8)*(Math.random()*4))
+    ld_perf.push((Math.random()*8)*(Math.random()*4))
+
+}
+
 $('#dateRange').val('selectedvalue').change(function() {
     if (this.value == "7") { 
         displayDates = dates.slice(dates.length - 7); 
@@ -105,7 +147,20 @@ $('#dateRange').val('selectedvalue').change(function() {
     setChart(currIndex);
 });
 
-var	datasets = [
+$('#type').val('selectedvalue').change(function() {
+    if (this.value == "gen") {
+        displayDates = dates.slice(dates.length - 7);
+        $("#dateRange").val('selectedvalue').val("7");
+        setChart(0); 
+    } else if (this.value == "per") {
+        displayDates = dates.slice(dates.length - 7);
+        $("#dateRange").val('selectedvalue').val("7");
+        setChart(1); 
+    }
+
+});
+
+var	chart1_dataset = [
 	{
 		label: '3rd Attempt',
         data: try3,
@@ -244,13 +299,107 @@ var	datasets = [
     }
 ]
 
+var chart2_dataset = [
+    {
+        label: '/h/',
+        data: h_perf,
+        borderColor: 'rgba(255, 0, 0, 1)',
+        fill: false
+    }, {
+        label: '/p/',
+        data: p_perf,
+        borderColor: 'rgba(255, 128, 0, 1)',
+        fill: false
+    } , {
+        label: '/b/',
+        data: b_perf,
+        borderColor: 'rgba(255, 191, 0, 1)',
+        fill: false
+    }, {
+        label: '/t/',
+        data: t_perf,
+        borderColor: 'rgba(255, 255, 0, 1)',
+        fill: false
+    }, {
+        label: '/d/',
+        data: d_perf,
+        borderColor: 'rgba(191, 255, 0, 1)',
+        fill: false
+    }, {
+        label: '/k/',
+        data: k_perf,
+        borderColor: 'rgba(128, 255, 0, 1)',
+        fill: false
+    }, {
+        label: '/g/',
+        data: g_perf,
+        borderColor: 'rgba(0, 255, 128, 1)',
+        fill: false
+    }, {
+        label: '/s/',
+        data: s_perf,
+        borderColor: 'rgba(0, 255, 255, 1)',
+        fill: false
+    }, {
+        label: '/Θ/',
+        data: th_perf,
+        borderColor: '  rgba(0, 128, 255, 1)',
+        fill: false
+    }, {
+        label: '/Θs/',
+        data: ths_perf,
+        borderColor: 'rgba(0, 0, 255, 1)',
+        fill: false
+    }, {
+        label: '/∫/',
+        data: sh_perf,
+        borderColor: 'rgba(128, 0, 255, 1)',
+        fill: false
+    }, {
+        label: '/f/',
+        data: f_perf,
+        borderColor: 'rgba(191, 0, 255, 1)',
+        fill: false
+    }, {
+        label: '/t∫/',
+        data: ch_perf,
+        borderColor: 'rgba(255, 0, 255, 1)',
+        fill: false
+    }, {
+        label: '/dʒ/',
+        data: j_perf,
+        borderColor: 'rgba(255, 0, 191, 1)',
+        fill: false
+    }, {
+        label: 'Final /nt/',
+        data: nt_perf,
+        borderColor: 'rgba(255, 0, 128, 1)',
+        fill: false
+    }, {
+        label: 'Final /nd/',
+        data: nd_perf,
+        borderColor: 'blue',
+        fill: false
+    }, {
+        label: 'Final /lt/',
+        data: lt_perf,
+        borderColor: 'yellow',
+        fill: false
+    }, {
+        label: 'Final /ld/',
+        data: ld_perf,
+        borderColor: 'rgba(255, 0, 0, 1)',
+        fill: false
+    }
+]
+
 // GRAPH 0: Accuracy and Difficulty Gradient
 var build0 = function () {
-	var myChart1 = new Chart(ctx, {
+	var myChart0 = new Chart(ctx, {
 		type: 'bar',
 		data: {
             labels: displayDates,
-            datasets: datasets
+            datasets: chart1_dataset
         },
 		options: {
 	        title: {
@@ -260,8 +409,8 @@ var build0 = function () {
 	            fontSize: 17
 	        },
 	        legend: {
-	            display: true,
-	            position: 'right'
+	            display: false,
+	            position: 'bottom'
 	        },
 	        scales: { 
         		yAxes: [{ 
@@ -279,11 +428,49 @@ var build0 = function () {
 	    	}
 	    }			
 	});
+    return myChart0;
+}
+
+var build1 = function() {
+    var myChart1 = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: displayDates,
+            datasets: chart2_dataset
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Performance',
+                position: 'top',
+                fontSize: 17
+            }, 
+            legend: {
+                display: true,
+                position: 'right'
+            },
+            scales: {
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Performance (difficulty * attempts)'
+                    }
+                }], xAxes: [{
+                    stacked: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date (MM/DD/YYYY)'
+                    }
+                }]
+            }
+        }
+    });
     return myChart1;
 }
 
 // Put all charts into chartList array
 chartList.push(build0);
+chartList.push(build1);
 
 // setChart: loads correct chart to page based on an index (1-4)
 function setChart(index) {
