dd2e70482e6793951e9fb40624cdc5560e0163b8
shleewhite <leewhite128@gmail.com>
2017-05-11 15:23:27 -0700

    changed overview graph to be weighted avg

--- new.js
+++ new.js
@@ -14,32 +14,7 @@ var try1 = []							// array of times successful on first attempt each day
 var try2 = []							// array of times successful on second attempt each day
 var try3 = []							// array of times successful on third attempt each day
 
-var try1_diff1 = []						// array of levels attempted at difficulty 1 (EASIEST)
-var try1_diff2 = []						// array of levels attempted at difficulty 2
-var try1_diff3 = []						// array of levels attempted at difficulty 3
-var try1_diff4 = []						// array of levels attempted at difficulty 4
-var try1_diff5 = []						// array of levels attempted at difficulty 5
-var try1_diff6 = []						// array of levels attempted at difficulty 6
-var try1_diff7 = []						// array of levels attempted at difficulty 7
-var try1_diff8 = []						// array of levels attempted at difficulty 8 (HARDEST)
-
-var try2_diff1 = []						// array of levels attempted at difficulty 1 (EASIEST)
-var try2_diff2 = []						// array of levels attempted at difficulty 2
-var try2_diff3 = []						// array of levels attempted at difficulty 3
-var try2_diff4 = []						// array of levels attempted at difficulty 4
-var try2_diff5 = []						// array of levels attempted at difficulty 5
-var try2_diff6 = []						// array of levels attempted at difficulty 6
-var try2_diff7 = []						// array of levels attempted at difficulty 7
-var try2_diff8 = []						// array of levels attempted at difficulty 8 (HARDEST)
-
-var try3_diff1 = []						// array of levels attempted at difficulty 1 (EASIEST)
-var try3_diff2 = []						// array of levels attempted at difficulty 2
-var try3_diff3 = []						// array of levels attempted at difficulty 3
-var try3_diff4 = []						// array of levels attempted at difficulty 4
-var try3_diff5 = []						// array of levels attempted at difficulty 5
-var try3_diff6 = []						// array of levels attempted at difficulty 6
-var try3_diff7 = []						// array of levels attempted at difficulty 7
-var try3_diff8 = []						// array of levels attempted at difficulty 8 (HARDEST)
+var w_avg = []                          // array of weighted average scores for each day
 
 var h_perf = []                         // arrays for performance based on what said
 var p_perf = []
@@ -77,40 +52,28 @@ for (var i = 0; i < dates.length; i++) {
 	try3.push(Math.floor(Math.random() * 15));
 }
 
-// calculate percentages for difficulty
-for (var i = 0; i < dates.length; ++i) {
-    var total = try1[i] / 8;
+for (var i = 0; i < dates.length; i++) {
+    var sum_1 = 0;
+    var diff_sum = 0;
+    for (var j = 0; j < try1[i]; j++) {
+        temp = Math.floor(Math.random() * 9);
+        sum_1 += temp;
+        diff_sum += temp;
+    }
 
-    try1_diff1.push(total);
-	try1_diff2.push(total);
-	try1_diff3.push(total);
-	try1_diff4.push(total);
-	try1_diff5.push(total);
-	try1_diff6.push(total);
-	try1_diff7.push(total);
-	try1_diff8.push(total);
+    for (var j = 0; j < try2[i]; j++) {
+        temp = Math.floor(Math.random() * 9);
+        sum_1 += 2 * temp;
+        diff_sum += temp;
+    }
 
-	total = try2[i] / 8;
+    for (var j = 0; j < try3[i]; j++) {
+        temp = Math.floor(Math.random() * 9);
+        sum_1 += 3 * temp;
+        diff_sum += temp;
+    }
 
-    try2_diff1.push(total);
-	try2_diff2.push(total);
-	try2_diff3.push(total);
-	try2_diff4.push(total);
-	try2_diff5.push(total);
-	try2_diff6.push(total);
-	try2_diff7.push(total);
-	try2_diff8.push(total);
-
-	total = try3[i] / 8;
-
-    try3_diff1.push(total);
-	try3_diff2.push(total);
-	try3_diff3.push(total);
-	try3_diff4.push(total);
-	try3_diff5.push(total);
-	try3_diff6.push(total);
-	try3_diff7.push(total);
-	try3_diff8.push(total);
+    w_avg.push(sum_1 / diff_sum);
 }
 
 // add values to performance arrays
@@ -160,142 +123,12 @@ $('#type').val('selectedvalue').change(function() {
 
 });
 
-var	chart1_dataset = [
-	{
-		label: '3rd Attempt',
-        data: try3,
-        backgroundColor: 'blue',
-        stack: 1
-	}, {
-		label: '2nd Attempt',
-        data: try2,
-        backgroundColor: 'rgba(0, 255, 250, 1)',
-        stack: 1
-	}, {
-		label: '1st Attempt',
-        data: try1,
-        backgroundColor: 'purple',
-        stack: 1
-	}, {
-        label: 'Third Attempt Difficulty 1',
-        data: try3_diff1,
-        backgroundColor: 'rgba(0, 255, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 2',
-        data: try3_diff2,
-        backgroundColor: 'rgba(102, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 3',
-        data: try3_diff3,
-        backgroundColor: 'rgba(204, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 4',
-        data: try3_diff4,
-        backgroundColor: 'rgba(255, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 5',
-        data: try3_diff5,
-        backgroundColor: 'rgba(255, 204, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 6',
-        data: try3_diff6,
-        backgroundColor: 'rgba(255, 153, 51, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 7',
-        data: try3_diff7,
-        backgroundColor: 'rgba(255, 102, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Third Attempt Difficulty 8',
-        data: try3_diff8,
-        backgroundColor: 'rgba(255, 0, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 1',
-        data: try2_diff1,
-        backgroundColor: 'rgba(0, 255, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 2',
-        data: try2_diff2,
-        backgroundColor: 'rgba(102, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 3',
-        data: try2_diff3,
-        backgroundColor: 'rgba(204, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 4',
-        data: try2_diff4,
-        backgroundColor: 'rgba(255, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 5',
-        data: try2_diff5,
-        backgroundColor: 'rgba(255, 204, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 6',
-        data: try2_diff6,
-        backgroundColor: 'rgba(255, 153, 51, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 7',
-        data: try2_diff7,
-        backgroundColor: 'rgba(255, 102, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'Second Attempt Difficulty 8',
-        data: try2_diff8,
-        backgroundColor: 'rgba(255, 0, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 1',
-        data: try1_diff1,
-        backgroundColor: 'rgba(0, 255, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 2',
-        data: try1_diff2,
-        backgroundColor: 'rgba(102, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 3',
-        data: try1_diff3,
-        backgroundColor: 'rgba(204, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 4',
-        data: try1_diff4,
-        backgroundColor: 'rgba(255, 255, 102, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 5',
-        data: try1_diff5,
-        backgroundColor: 'rgba(255, 204, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 6',
-        data: try1_diff6,
-        backgroundColor: 'rgba(255, 153, 51, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 7',
-        data: try1_diff7,
-        backgroundColor: 'rgba(255, 102, 0, 0.75)',
-        stack: 2
-    }, {
-        label: 'First Attempt Difficulty 8',
-        data: try1_diff8,
-        backgroundColor: 'rgba(255, 0, 0, 0.75)',
-        stack: 2
+var chart1_dataset = [
+    {
+        label: 'weighted avg',
+        data: w_avg,
+        borderColor: 'blue',
+        fill: false
     }
 ]
 
@@ -344,7 +177,8 @@ var chart2_dataset = [
         label: '/Θ/',
         data: th_perf,
         borderColor: '  rgba(0, 128, 255, 1)',
-        fill: false
+        fill: false,
+        hidden: true
     }, {
         label: '/Θs/',
         data: ths_perf,
@@ -405,7 +239,7 @@ var chart2_dataset = [
 // GRAPH 0: Accuracy and Difficulty Gradient
 var build0 = function () {
 	var myChart0 = new Chart(ctx, {
-		type: 'bar',
+		type: 'line',
 		data: {
             labels: displayDates,
             datasets: chart1_dataset
@@ -418,12 +252,11 @@ var build0 = function () {
 	            fontSize: 17
 	        },
 	        legend: {
-	            display: false,
-	            position: 'bottom'
+	            display: true,
+	            position: 'right'
 	        },
 	        scales: { 
         		yAxes: [{ 
-	        		stacked: true,
 	        		scaleLabel: {
 	        			display: true,
 	        			labelString: 'Time Spent Speaking (in Minutes)'
